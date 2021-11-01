dfa364a8335469c7849bf3e1f2dd3782fcbb9218
HanfiyZQ <zamaksyaryhanfiy@gmail.com>
2021-11-01 16:48:18 +0700

    update commit

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,8 @@ Route::post("/animals", [AnimalController::class, 'store']);
 Route::put("/animals/{id}", [AnimalController::class, 'update']);
 
 Route::delete("/animals/{id}", [AnimalController::class, 'destroy']);
+
+#Route Students
+
+Route::get("/students", [StudentController::class, 'index']);
+Route::post("/students", [StudentController::class, 'store']);
