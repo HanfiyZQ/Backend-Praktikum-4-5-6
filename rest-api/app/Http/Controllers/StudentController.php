7d878ac612c171afc7afccd2f43becc310b5e7a3
HanfiyZQ <zamaksyaryhanfiy@gmail.com>
2021-11-02 20:43:09 +0700

    Student Controller: add update and destroy resource

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -38,4 +38,82 @@ class StudentController extends Controller
 
         return response()->json($data, 201);
     }
+
+    #method show
+
+    public function show($id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $data = [
+                'message' => 'Get detail student',
+                'data' => $student
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Data is not found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    #method update
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan
+            ];
+
+            $student->update($input);
+
+            $data = [
+                'messange' => 'Data is updated',
+                'data' => $student
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Data is not found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    #method delete
+
+    public function destroy($id)
+    {
+
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->delete();
+
+            $data = [
+                'message' => 'Data is deleted'
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Data is not found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
 }
--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -20,18 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+#Route Animals
 
-#method GET
 Route::get("/animals", [AnimalController::class, 'index']);
-
-#methode POST
 Route::post("/animals", [AnimalController::class, 'store']);
-
 Route::put("/animals/{id}", [AnimalController::class, 'update']);
-
 Route::delete("/animals/{id}", [AnimalController::class, 'destroy']);
 
 #Route Students
 
 Route::get("/students", [StudentController::class, 'index']);
 Route::post("/students", [StudentController::class, 'store']);
+Route::get("/students/{id}", [StudentController::class, 'show']);
+Route::put("/students/{id}", [StudentController::class, 'update']);
+Route::delete("/students/{id}", [StudentController::class, 'destroy']);
